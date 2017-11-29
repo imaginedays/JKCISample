67e6f5df43ab43c36946b2162c7ea0d4d5ce9a14
imaginedays <imaginedays@huangkedeMacBook-Pro.local>
2017-11-29 14:34:22 +0800

    Initial commit

---
+++ JKCISample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JKCISample
+//
+//  Created by imaginedays on 29/11/2017.
+//  Copyright © 2017 imaginedays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JKCISample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JKCISample
+//
+//  Created by imaginedays on 29/11/2017.
+//  Copyright © 2017 imaginedays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
